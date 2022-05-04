ccdef3598722d22ab62a63246b4667dfa942f699
Fron <syper.martunenko200@yandex.ru>
2022-05-04 20:15:01 +0300

    bug fix

--- my-app/src/components/mockdata/WeatherIconIdentifier.js
+++ my-app/src/components/mockdata/WeatherIconIdentifier.js
@@ -2,10 +2,12 @@ import cloudy from '../../assests/cloudy.png'
 import sun from '../../assests/sun.png'
 import rain from '../../assests/rain.png'
 import snow from '../../assests/snow.png'
+import tornado from '../../assests/tornado.png'
 
 export const weatherIcon = {
   Clouds: cloudy,
   Clear: sun,
   Rain: rain,
   Snow: snow,
+  Dust: tornado
 }
